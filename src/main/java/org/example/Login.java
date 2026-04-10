5530a05c48b99ba714c1b98b0c5bc87a2ed83dd6
Sophycoder519 <ST10479223@rcconnect.edu.za>
2026-04-10 14:21:07 +0200

    Initial project setup with Login and Main classes

---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Login {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String phoneNumber;
+}
